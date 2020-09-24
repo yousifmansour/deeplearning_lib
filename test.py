681028e06302ef4e209f239b62ea68b12b017e1d
yousif <yousifmansour@protonmail.com>
2020-09-23 22:04:59 -0400

    some file changes and split dataset

--- test.py
+++ test.py
@@ -2,31 +2,46 @@ import numpy as np
 import tensorflow as tf
 from sklearn import datasets
 
-from core.logistic_regression import regression
-from core.neural_network_2_layer import nn_2_layer
-from core.neural_network.neural_network import nn_l_layer
+from core.neural_network import neural_network
+from core.simple.logistic_regression import regression
+from core.simple.neural_network_2_layer import nn_2_layer
+from core.preprocessing.split_dataset import split_dataset
+from core.cost.logistic import calc_precision, __logistic_cost
 
 X, y = datasets.load_breast_cancer(return_X_y=True)
 
 
-def nnlib_regression():
+def nnlib_regression(X, y):
     predictions, error, acc = regression(X.T, y, 10000)
     print(1*(predictions > 0.5))
     print("Accuracy = ", acc, '%')
 
 
-def nnlib_nn_2_layer():
+def nnlib_nn_2_layer(X, y):
     predictions, error, acc = nn_2_layer(X.T, y, 10000, 16, 0.001)
     print(1*(predictions > 0.5))
     print("Accuracy = ", acc, '%')
 
 
-def nnlib_nn_l_layer():
-    predictions, error, acc = nn_l_layer(X.T, y, 10000, [4, 64, 8, 1], 0.001)
+def nnlib_nn_l_layer(X, y):
+    predictions, error, acc = neural_network.train(
+        X.T, y, 100000, [3, 4, 3, 1], 0.01)
     print(1*(predictions > 0.5))
     print("Accuracy = ", acc, '%')
 
-
 # nnlib_regression()
 # nnlib_nn_2_layer()
-nnlib_nn_l_layer()
+
+
+X_train, X_dev, X_test, y_train, y_dev, y_test = split_dataset(X, y, 0.6, 0.2)
+network_layers = [8, 6, 4, 3, 1]
+parameters, predictions, error, acc = neural_network.train(
+    X.T, y, 100000, network_layers, 0.05)
+
+predictions = neural_network.predict(
+    X_dev.T, parameters, len(network_layers) - 1)
+dev_error = __logistic_cost(predictions, y_dev, y_dev[0])
+dev_acc = calc_precision(predictions, y_dev)
+
+print("Train:\n\tError=", error, "\n\tAccuracy=", acc, '%')
+print("Dev:\n\tError=", dev_error, "\n\tAccuracy=", dev_acc, '%')
