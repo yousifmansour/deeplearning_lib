67013c6691f71665c8b34d94ccb1a9189fad55f8
yousif <yousifmansour@protonmail.com>
2020-09-30 22:04:18 -0400

    add momentum

--- test.py
+++ test.py
@@ -8,34 +8,45 @@ from core.preprocessing.normalize_inputs import apply_normalize, normalize
 from core.preprocessing.split_dataset import split_dataset
 
 X, y = datasets.load_breast_cancer(return_X_y=True)
+np.random.seed(1)
 
 
 m = X.shape[0]
 y.reshape(m, 1)
-iterations = 30000
 
 
-def learning_rate(iteration): 
-    return 0.01 if iteration < 5000 else 0.001
+epochs = 100
 
 
-keep_prob = 1
-lambd = 0.5
+def learning_rate(epoch):
+    return (0.05 if epoch < epochs else 0.01)
 
-hidden_units = 8
+
+keep_prob = 0.6
+lambd = 0
+
+hidden_units = 1024
+
+optimization_alg = "momentum"
+beta_1 = 0.9
+
+hidden_layer = {"units": hidden_units, "activation": 'relu', "keep_prob": keep_prob, "lambd": lambd,
+                "learning_rate": learning_rate, "optimization": {"alg": optimization_alg, "beta_1": beta_1}}
 
 layers = [
     {"units": hidden_units, "activation": 'relu', "keep_prob": 1,
-        "lambd": lambd, "learning_rate": learning_rate},
+     "lambd": lambd, "learning_rate": learning_rate,
+     "optimization": {"alg": optimization_alg, "beta_1": beta_1}
+     },
 
-    {"units": hidden_units, "activation": 'relu', "keep_prob": 1,
-     "lambd": lambd, "learning_rate": learning_rate},
-    {"units": hidden_units, "activation": 'relu', "keep_prob": 1,
-     "lambd": lambd, "learning_rate": learning_rate},
-    {"units": hidden_units, "activation": 'relu', "keep_prob": 1,
-     "lambd": lambd, "learning_rate": learning_rate},
+    hidden_layer,
+    hidden_layer,
+    hidden_layer,
+    hidden_layer,
 
-    {"units": 1, "activation": 'sigmoid', "keep_prob": 1, "lambd": lambd, "learning_rate": learning_rate}]
+    {"units": 1, "activation": 'sigmoid', "keep_prob": 1, "lambd": lambd, "learning_rate": learning_rate,
+     "optimization": {"alg": optimization_alg, "beta_1": beta_1}
+     }, ]
 
 X_train, X_dev, X_test, y_train, y_dev, y_test = split_dataset(
     X, y, True, 0.5, 0.25)
@@ -44,7 +55,7 @@ X_dev = apply_normalize(X_dev, X_mean, X_standard_deviation)
 X_test = apply_normalize(X_test, X_mean, X_standard_deviation)
 
 parameters, predictions, error, acc = neural_network.train(
-    X_train.T, y_train, X_dev.T, y_dev, iterations=iterations, batch_size=64, layers=layers)
+    X_train.T, y_train, X_dev.T, y_dev, epochs=epochs, batch_size=64, layers=layers)
 
 dev_predictions = neural_network.predict(X_dev.T, parameters, layers)
 dev_error = __logistic_cost(dev_predictions, y_dev, parameters, 0, len(layers))
@@ -53,10 +64,11 @@ dev_acc = calc_f1_score(dev_predictions, y_dev)
 print("Train:\n\tError=", error, "\n\tF1 Accuracy=", acc, '%')
 print("Dev:\n\tError=", dev_error, "\n\tF1 Accuracy=", dev_acc, '%')
 
-test_predictions = neural_network.predict(X_test.T, parameters, layers)
-test_error = __logistic_cost(test_predictions, y_test, parameters, 0, len(layers))
-test_acc = calc_f1_score(test_predictions, y_test)
-print("Test:\n\tError=", test_error, "\n\tF1 Accuracy=", test_acc, '%')
+# test_predictions = neural_network.predict(X_test.T, parameters, layers)
+# test_error = __logistic_cost(
+#     test_predictions, y_test, parameters, 0, len(layers))
+# test_acc = calc_f1_score(test_predictions, y_test)
+# print("Test:\n\tError=", test_error, "\n\tF1 Accuracy=", test_acc, '%')
 
 # print(1*(predictions.T[10: 20].T > 0.5))
 # print(y[10: 20])
